b1acf09b802fa0bfc06aeca6c7fa233a8a6db16b
Noah Kulas <kulasnoah9@gmail.com>
2020-01-12 15:32:28 -0600

    Added bitwise processing

--- ToHash/ToHash.cpp
+++ ToHash/ToHash.cpp
@@ -1,5 +1,7 @@
 // ToHash.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//Created by Noah Kulas, 2019
+//Some parts were taken from https://en.wikipedia.org/wiki/MD5
 
 #include <iostream>
 #include <conio.h>
@@ -7,7 +9,10 @@
 #include <string>
 
 bool* ConvertToBinary(int, int);
+bool* ConvertToBinary(long long, int);
 void AddBitsToVector(bool*, std::vector<bool>*, int);
+bool* SliceVector(std::vector<bool>*, int, int);
+//bool ConvertVectorToArray(std::vector<bool>*);
 
 int main()
 {
@@ -45,62 +50,72 @@ int main()
 	}
 	
 	//Add 8 octets representing the length to the end
-	AddBitsToVector(ConvertToBinary(userInput.length()*8, 64), p_mainMessageInBinary, 64);
-	
-	//Current termination point
-	//The bits of the message to this point are written
-	for (int i = 0; i < 512; i++) {
-		std::cout << p_mainMessageInBinary->at(i);
-	}
-	exit;
+	AddBitsToVector(ConvertToBinary((int)userInput.length()*8, 64), p_mainMessageInBinary, 64);
 
-	//Initialize buffers
-	std::vector<bool*> bufferA;
-	std::vector<bool*> bufferB;
-	std::vector<bool*> bufferC;
-	std::vector<bool*> bufferD;
+	//Constants that will be used in buffers
+	const bool* A_VALUE = ConvertToBinary((int)0x67452301,32);
+	const bool* B_VALUE = ConvertToBinary((int)0xefcdab89, 32);
+	const bool* C_VALUE = ConvertToBinary((int)0x98badcfe, 32);
+	const bool* D_VALUE = ConvertToBinary((int)0x10325476, 32);
 
-	bufferA.push_back(ConvertToBinary(0x01,8));
-	bufferA.push_back(ConvertToBinary(0x23,8));
-	bufferA.push_back(ConvertToBinary(0x45,8));
-	bufferA.push_back(ConvertToBinary(0x67,8));
-
-	bufferB.push_back(ConvertToBinary(0x89,8));
-	bufferB.push_back(ConvertToBinary(0xab,8));
-	bufferB.push_back(ConvertToBinary(0xcd,8));
-	bufferB.push_back(ConvertToBinary(0xef,8));
-
-	bufferC.push_back(ConvertToBinary(0xfe,8));
-	bufferC.push_back(ConvertToBinary(0xdc,8));
-	bufferC.push_back(ConvertToBinary(0xba,8));
-	bufferC.push_back(ConvertToBinary(0x98,8));
-
-	bufferD.push_back(ConvertToBinary(0x76,8));
-	bufferD.push_back(ConvertToBinary(0x54,8));
-	bufferD.push_back(ConvertToBinary(0x32,8));
-	bufferD.push_back(ConvertToBinary(0x10,8));
-
-	//Begin working with the message
-
-	//foreach 512 bit chunk in the message
-
-	//Get the first 512 bit chunk
-	std::vector<bool*> currentLargeChunk;
+	//Constants that will be added to message
+	std::vector<bool*>* K = new std::vector<bool*>;
 	for (int i = 0; i < 64; i++) {
-		//currentLargeChunk.push_back(binaryMessage[i]);
+		K->push_back(
+			ConvertToBinary(
+				(long long)(floor(pow(2, 32) * abs(sin(i + 1)))),
+			32)
+		);
 	}
 
-	std::vector<bool> chunkA;
-	std::vector<bool> chunkB;
-	std::vector<bool> chunkC;
-	std::vector<bool> chunkD;
+	//foreach 512 bit chunk in the message {
+
+		//Get the first 512 bit chunk
+		bool* FivtwelChunk = SliceVector(p_mainMessageInBinary, 0, 511);
+
+		//Break this into 32 bit chuncks
+		std::vector<bool*>* MessageHolder = new std::vector<bool*>;
+		for (int i = 0; i < 512; i += 32) {
+			bool* ThretoChunk = new bool[32];
+			for (int j = 0; j < 32; j++) {
+				ThretoChunk[j] = FivtwelChunk[i + j];
+			}
+			MessageHolder->push_back(ThretoChunk);
+		}
+
+		//Set buffer values
+		bool bufferA = *A_VALUE;
+		bool bufferB = *B_VALUE;
+		bool bufferC = *C_VALUE;
+		bool bufferD = *D_VALUE;
+
+		//Do the actual operations
+		bool f;
+		int m;
+		for (int i = 0; i < 64; i++) {
+			if (i >= 0 && i <= 15) {
+				f = ((bufferB & bufferC) | ((~bufferB) & bufferD));
+				m = i;
+			}
+			else if (i >= 16 && i <= 31) {
+				f = ((bufferD & bufferB) | ((~bufferD) & bufferC));
+				m = (((5 * i) + 1) % 16);
+			}
+			else if (i >= 32 && i <= 47) {
+				f = (bufferB ^ bufferC ^ bufferD);
+				m = (((3 * i) + 5) % 16);
+			}
+			else if (i >= 48 && i <= 63) {
+				f = (bufferC ^ (bufferB | (~bufferD)));
+				m = ((7 * i) % 16);
+			}
+		}
 	//}
 }
 
 //Support functions
 
 //Takes a decimal number and returns a pointer to an 8 bit array
-
 bool* ConvertToBinary(int decimalNumber, int numberOfBits) {
 	//Convert to binary
 	bool* binary = new bool[numberOfBits];
@@ -119,8 +134,47 @@ bool* ConvertToBinary(int decimalNumber, int numberOfBits) {
 	return binary;
 }
 
+bool* ConvertToBinary(long long decimalNumber, int numberOfBits) {
+	//Convert to binary
+	bool* binary = new bool[numberOfBits];
+
+	//Create a vector representing 8 bits
+	for (int x = 0; x < numberOfBits; x++) {
+		*(binary + x) = 0;
+	}
+
+	//For each bit, calculate it binary value, determine if this can be pulled out of the decimal number
+	for (int j = (numberOfBits - 1); j >= 0; j--) {
+		long long twoMultiple = pow(2, j);
+		binary[(numberOfBits - 1) - j] = (int)floor(decimalNumber / twoMultiple);
+		decimalNumber = decimalNumber % twoMultiple;
+	}
+	return binary;
+}
+
+//Takes a pointer to an array of bits and adds them to a vector
 void AddBitsToVector(bool* bits, std::vector<bool>* v, int numberOfBits) {
 	for (int i = 0; i < numberOfBits; i++) {
 		v->push_back(bits[i]);
 	}
 }
+
+//Creates an array from part of a vector
+bool* SliceVector(std::vector<bool>* v, int startIndex, int endIndex) {
+	bool* returnArray = new bool[endIndex - startIndex + 1];
+
+	for (int i = startIndex; i <= endIndex; i++) {
+		returnArray[i - startIndex] = v->at(i);
+	}
+	return returnArray;
+}
+
+//Takes a vector and makes an array
+/*bool ConvertVectorToArray(std::vector<bool>* v) {
+	bool* returnArray = new bool[v->size];
+
+	for (int i = 0; i < v->size; i++) {
+		returnArray[i] = v->at(i);
+	}
+	return returnArray;
+}*/
