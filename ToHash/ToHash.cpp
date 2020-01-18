d2140d9b14b91f91adf4f6bbe0b656783737a98e
Noah Kulas <kulasnoah9@gmail.com>
2020-01-18 12:07:30 -0600

    Added actual hashing and started modular add

--- ToHash/ToHash.cpp
+++ ToHash/ToHash.cpp
@@ -8,10 +8,12 @@
 #include <vector>
 #include <string>
 
-bool* ConvertToBinary(int, int);
-bool* ConvertToBinary(long long, int);
-void AddBitsToVector(bool*, std::vector<bool>*, int);
-bool* SliceVector(std::vector<bool>*, int, int);
+uint8_t* ConvertToBinary(int, int);
+uint8_t* ConvertToBinary(long long, int);
+void AddBitsToVector(uint8_t*, std::vector<uint8_t>*, int);
+uint8_t* SliceVector(std::vector<uint8_t>*, int, int);
+void LeftRotate(uint8_t*, int);
+uint8_t* ModularBinaryAdd(uint8_t*, uint8_t*);
 //bool ConvertVectorToArray(std::vector<bool>*);
 
 int main()
@@ -22,11 +24,11 @@ int main()
 	std::getline(std::cin, userInput);
 
 	//Convert the message to binary
-	std::vector<bool>* p_mainMessageInBinary = new std::vector<bool>();
+	std::vector<uint8_t>* p_mainMessageInBinary = new std::vector<uint8_t>();
 	
 	for (int i = 0; i < userInput.length(); i++) {
 		int charaterInAsciiDecimal = (int)userInput[i];
-		bool* p_charaterInBinary = ConvertToBinary(charaterInAsciiDecimal,8);
+		uint8_t* p_charaterInBinary = ConvertToBinary(charaterInAsciiDecimal,8);
 
 		AddBitsToVector(p_charaterInBinary, p_mainMessageInBinary, 8);
 	}
@@ -53,13 +55,14 @@ int main()
 	AddBitsToVector(ConvertToBinary((int)userInput.length()*8, 64), p_mainMessageInBinary, 64);
 
 	//Constants that will be used in buffers
-	const bool* A_VALUE = ConvertToBinary((int)0x67452301,32);
-	const bool* B_VALUE = ConvertToBinary((int)0xefcdab89, 32);
-	const bool* C_VALUE = ConvertToBinary((int)0x98badcfe, 32);
-	const bool* D_VALUE = ConvertToBinary((int)0x10325476, 32);
+	//Values in hex are: A: 01234567, B: 89abcdef, C: fedcba98, D: 76543210
+	const uint8_t* A_VALUE = ConvertToBinary((int)0x67452301,32);
+	const uint8_t* B_VALUE = ConvertToBinary((int)0xefcdab89, 32);
+	const uint8_t* C_VALUE = ConvertToBinary((int)0x98badcfe, 32);
+	const uint8_t* D_VALUE = ConvertToBinary((int)0x10325476, 32);
 
 	//Constants that will be added to message
-	std::vector<bool*>* K = new std::vector<bool*>;
+	std::vector<uint8_t*>* K = new std::vector<uint8_t*>;
 	for (int i = 0; i < 64; i++) {
 		K->push_back(
 			ConvertToBinary(
@@ -68,15 +71,31 @@ int main()
 		);
 	}
 
+	//Running hash
+	uint8_t* HashA = new uint8_t[32];
+	uint8_t* HashB = new uint8_t[32];
+	uint8_t* HashC = new uint8_t[32];
+	uint8_t* HashD = new uint8_t[32];
+
+	for (int i = 0; i < 32; i++) {
+		HashA[i] = A_VALUE[i];
+		HashB[i] = B_VALUE[i];
+		HashC[i] = C_VALUE[i];
+		HashD[i] = D_VALUE[i];
+	}
+
+	//Shift amounts for each round
+	int* ShiftAmount = new int[64]{ 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 };
+
 	//foreach 512 bit chunk in the message {
 
 		//Get the first 512 bit chunk
-		bool* FivtwelChunk = SliceVector(p_mainMessageInBinary, 0, 511);
+		uint8_t* FivtwelChunk = SliceVector(p_mainMessageInBinary, 0, 511);
 
 		//Break this into 32 bit chuncks
-		std::vector<bool*>* MessageHolder = new std::vector<bool*>;
+		std::vector<uint8_t*>* MessageHolder = new std::vector<uint8_t*>;
 		for (int i = 0; i < 512; i += 32) {
-			bool* ThretoChunk = new bool[32];
+			uint8_t* ThretoChunk = new uint8_t[32];
 			for (int j = 0; j < 32; j++) {
 				ThretoChunk[j] = FivtwelChunk[i + j];
 			}
@@ -84,41 +103,93 @@ int main()
 		}
 
 		//Set buffer values
-		bool bufferA = *A_VALUE;
-		bool bufferB = *B_VALUE;
-		bool bufferC = *C_VALUE;
-		bool bufferD = *D_VALUE;
+		uint8_t* bufferA = new uint8_t[32];
+		uint8_t* bufferB = new uint8_t[32];
+		uint8_t* bufferC = new uint8_t[32];
+		uint8_t* bufferD = new uint8_t[32];
 
-		//Do the actual operations
-		bool f;
+		for (int i = 0; i < 32; i++) {
+			bufferA[i] = A_VALUE[i];
+			bufferB[i] = B_VALUE[i];
+			bufferC[i] = C_VALUE[i];
+			bufferD[i] = D_VALUE[i];
+		}
+
+		//Put the buffers through the transform functions
+		uint8_t* f = new uint8_t[32];
 		int m;
 		for (int i = 0; i < 64; i++) {
-			if (i >= 0 && i <= 15) {
-				f = ((bufferB & bufferC) | ((~bufferB) & bufferD));
-				m = i;
+			for (int j = 0; j < 32; j++) {
+				if (i >= 0 && i <= 15) {
+					f[j] = ((bufferB[j] & bufferC[j]) | ((~bufferB[j]) & bufferD[j]));
+					m = i;
+				}
+				else if (i >= 16 && i <= 31) {
+					f[j] = ((bufferD[j] & bufferB[j]) | ((~bufferD[j]) & bufferC[j]));
+					m = (((5 * i) + 1) % 16);
+				}
+				else if (i >= 32 && i <= 47) {
+					f[j] = (bufferB[j] ^ bufferC[j] ^ bufferD[j]);
+					m = (((3 * i) + 5) % 16);
+				}
+				else if (i >= 48 && i <= 63) {
+					f[j] = (bufferC[j] ^ (bufferB[j] | (~bufferD[j])));
+					m = ((7 * i) % 16);
+				}
 			}
-			else if (i >= 16 && i <= 31) {
-				f = ((bufferD & bufferB) | ((~bufferD) & bufferC));
-				m = (((5 * i) + 1) % 16);
+
+			//Bitwise add bufferA, the result of the above functions, the appropriate constant, and the appropriate message block
+			for (int k = 0; k < 32; k++) {
+				uint8_t* rendom2 = ModularBinaryAdd(K->at(i), MessageHolder->at(m));
+				uint8_t* rendom1 = ModularBinaryAdd(bufferA, rendom2);
+				f = ModularBinaryAdd(f, rendom1);
 			}
-			else if (i >= 32 && i <= 47) {
-				f = (bufferB ^ bufferC ^ bufferD);
-				m = (((3 * i) + 5) % 16);
-			}
-			else if (i >= 48 && i <= 63) {
-				f = (bufferC ^ (bufferB | (~bufferD)));
-				m = ((7 * i) % 16);
+
+			LeftRotate(f, ShiftAmount[i]);
+
+			//Rotate the buffers
+			for (int m = 0; m < 32; m++) {
+				bufferA[m] = bufferD[m];
+				bufferD[m] = bufferC[m];
+				bufferC[m] = bufferB[m];
+				bufferB[m] = bufferB[m] + f[m];
 			}
 		}
+
+		//Add the result of this 64 operation round to the running hash
+		for (int i = 0; i < 32; i++) {
+			HashA = ModularBinaryAdd(HashA, bufferA);
+			HashB = ModularBinaryAdd(HashB, bufferB);
+			HashC = ModularBinaryAdd(HashC, bufferC);
+			HashD = ModularBinaryAdd(HashD, bufferD);
+		}
 	//}
+
+	//FOR TESTING
+	//Possibly the final result
+	for (int z = 0; z < 32; z++) {
+		std::cout << (double)HashA[z];
+	}
+
+	for (int z = 0; z < 32; z++) {
+		std::cout << (double)HashB[z];
+	}
+
+	for (int z = 0; z < 32; z++) {
+		std::cout << (double)HashC[z];
+	}
+
+	for (int z = 0; z < 32; z++) {
+		std::cout << (double)HashD[z];
+	}
 }
 
 //Support functions
 
 //Takes a decimal number and returns a pointer to an 8 bit array
-bool* ConvertToBinary(int decimalNumber, int numberOfBits) {
+uint8_t* ConvertToBinary(int decimalNumber, int numberOfBits) {
 	//Convert to binary
-	bool* binary = new bool[numberOfBits];
+	uint8_t* binary = new uint8_t[numberOfBits];
 
 	//Create a vector representing 8 bits
 	for (int x = 0; x < numberOfBits; x++) {
@@ -134,9 +205,9 @@ bool* ConvertToBinary(int decimalNumber, int numberOfBits) {
 	return binary;
 }
 
-bool* ConvertToBinary(long long decimalNumber, int numberOfBits) {
+uint8_t* ConvertToBinary(long long decimalNumber, int numberOfBits) {
 	//Convert to binary
-	bool* binary = new bool[numberOfBits];
+	uint8_t* binary = new uint8_t[numberOfBits];
 
 	//Create a vector representing 8 bits
 	for (int x = 0; x < numberOfBits; x++) {
@@ -153,20 +224,101 @@ bool* ConvertToBinary(long long decimalNumber, int numberOfBits) {
 }
 
 //Takes a pointer to an array of bits and adds them to a vector
-void AddBitsToVector(bool* bits, std::vector<bool>* v, int numberOfBits) {
+void AddBitsToVector(uint8_t* bits, std::vector<uint8_t>* v, int numberOfBits) {
 	for (int i = 0; i < numberOfBits; i++) {
 		v->push_back(bits[i]);
 	}
 }
 
 //Creates an array from part of a vector
-bool* SliceVector(std::vector<bool>* v, int startIndex, int endIndex) {
-	bool* returnArray = new bool[endIndex - startIndex + 1];
+uint8_t* SliceVector(std::vector<uint8_t>* v, int startIndex, int endIndex) {
+	uint8_t* returnArray = new uint8_t[endIndex - startIndex + 1];
 
 	for (int i = startIndex; i <= endIndex; i++) {
 		returnArray[i - startIndex] = v->at(i);
 	}
 	return returnArray;
+}
+
+uint8_t* ModularBinaryAdd(uint8_t* number1, uint8_t* number2) {
+	uint8_t* returnArray = new uint8_t[32];
+	bool carry = false;
+
+	for (int i = 31; i >= 0; i--) {
+		uint8_t result = number1[i] + number2[i];
+
+		if (carry) result += 1;
+
+		returnArray[i] = result % 2;
+
+		if (result > 1) carry = true;
+		else carry = false;
+	}
+
+	//TODO modulus is not working right
+	
+	bool continueMod = true;
+
+	while (continueMod) {
+		bool doSubtract = false;
+
+		if (carry) {
+			carry = false;
+			returnArray[0] = 1;
+			doSubtract = true;
+		}
+		else if (returnArray[0] == 1) {
+			doSubtract = true;
+		}
+		else {
+			continueMod = false;
+		}
+
+		if (doSubtract) {
+			//Create the modulus comparison
+			uint8_t* modulus = new uint8_t[32]{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+			//Do the subtraction
+			for (int i = 31; i >= 0; i--) {
+				if (returnArray[i] >= modulus[i]) {
+					returnArray[i] = returnArray[i] - modulus[i];
+				}
+				else {
+					int counter = 1;
+					bool continueBorrow = true;
+
+					while (continueBorrow) {
+						if (returnArray[i + counter] >= modulus[i]) {
+							returnArray[i + counter] -= 1;
+							returnArray[i] += 1;
+							continueBorrow = false;
+						}
+						else {
+							counter += 1;
+						}
+					}
+					returnArray[i] = returnArray[i] - modulus[i];
+				}
+			}
+		}
+
+		return returnArray;
+	}
+}
+
+void LeftRotate(uint8_t* thingToRotate, int numberOfBits) {
+	for (int i = 0; i < numberOfBits; i++) {
+		uint8_t temporaryHolder = thingToRotate[0];
+
+		for (int j = 0; j < 32; j++) {
+			if (j < 31) {
+				thingToRotate[j] = thingToRotate[j + 1];
+			}
+			else {
+				thingToRotate[j] = temporaryHolder;
+			}
+		}
+	}
 }
 
 //Takes a vector and makes an array
